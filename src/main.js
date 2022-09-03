57b38fd215f97920a92e2ee53408bba0d8e83100
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-09-03 12:04:24 +0900

    amplify apijunbi

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+// aws
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import AmplifyVue from '@aws-amplify/ui-vue'
+Amplify.configure(awsExports);
+loadFonts()
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(AmplifyVue)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
