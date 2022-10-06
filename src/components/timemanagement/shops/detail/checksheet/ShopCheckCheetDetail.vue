06ae7f7c95eda5a309a7b303357f76db1d2f612c
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-10-07 08:33:46 +0900

    店舗チェックシート表追加

--- src/components/timemanagement/shops/detail/checksheet/ShopCheckCheetDetail.vue
+++ src/components/timemanagement/shops/detail/checksheet/ShopCheckCheetDetail.vue
@@ -17,6 +17,51 @@
       style="width: 200px;"
     />
   </div>
+  <div class="my-2">
+    <v-table 
+      fixed-header
+      class="table-border"
+    >
+      <thead>
+        <tr>
+          <th>従業員</th>
+          <th>マニュアル通りの帽子のかぶり方、耳以外のピアスはないか</th>
+          <th>ネイル、装飾品、爪の長さは適正か</th>
+          <th>名札の着用</th>
+          <th>規定の制服の着衣が出来ているか</th>
+          <th>携帯の所持、ポケットから見える財布が無いか</th>
+          <th>下痢・発熱等はないか</th>
+          <th>手にケガ・火傷が無いか</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>大西 純平</td>
+          <td class="text-center">
+            <v-icon color="primary">mdi-check</v-icon>
+          </td>
+          <td class="text-center">
+            <v-icon color="primary">mdi-check</v-icon>
+          </td>
+          <td class="text-center">
+            <v-icon color="primary">mdi-check</v-icon>
+          </td>
+          <td class="text-center">
+            <v-icon color="primary">mdi-check</v-icon>
+          </td>
+          <td class="text-center">
+            <v-icon color="primary">mdi-check</v-icon>
+          </td>
+          <td class="text-center">
+            <v-icon color="primary">mdi-check</v-icon>
+          </td>
+          <td class="text-center">
+            <v-icon color="primary">mdi-check</v-icon>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 
 <script>
@@ -40,3 +85,20 @@ export default {
   },
 }
 </script>
+<style scoped>
+.v-table {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+}
+.v-table > .v-table__wrapper > table {
+  border-collapse: collapse;
+}
+table tr {
+  border-top: 1px solid #ddd;
+}
+table th,
+table td {
+  border-left: 1px solid #ddd;
+}
+</style>
